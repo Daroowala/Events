02e6f65f6a96f960b3f7183a7b9e2d2e7057f0fc
my name <blueshaperd3@gmail.com>
2024-05-02 01:08:03 +0500

    Event link

---
+++ app.js
@@ -0,0 +1,12 @@
+function Greet(){
+    alert("javascript mai function sai onclik par function call karna ka tariqa")
+}
+function foo(){
+    var a = 2 + 2;
+    var b = 3 * 3;
+    var c = a + b;
+    alert(c);
+}
+function too(Greet){
+    alert(Greet)
+}
